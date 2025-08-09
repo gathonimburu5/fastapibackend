1b283f52d5b789f991329ea28554758c05f09a96
Gathoni Mburu <gathonimburu5@gmail.com>
2025-08-09 23:27:26 +0300

    updating the utility with upload file

---
+++ application/utility/files.py
@@ -0,0 +1,19 @@
+import os
+import uuid
+from fastapi import UploadFile
+from pathlib import Path
+
+UPLOAD_FOLDER = Path("uploads/customerFiles")
+UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+
+def saveFiles(file: UploadFile):
+    folder_path = UPLOAD_FOLDER
+    folder_path.mkdir(parents=True, exist_ok=True)
+
+    file_ext = os.path.splitext(file.filename)[1]
+    unique_name = f"{uuid.uuid4()}{file_ext}"
+    file_path = folder_path/unique_name
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(file.read())
+    return str(file_path)
